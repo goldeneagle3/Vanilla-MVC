e25ea45ddfeeb5eeb02652da013a61201f32ee26
Football Worker <contacttofw@gmail.com>
2022-02-10 14:06:29 +0300

    first load

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '141ee029-36f2-4a70-a6d3-6633d648a7d6';
+export const MODAL_CLOSE_SEC = 2.5;
